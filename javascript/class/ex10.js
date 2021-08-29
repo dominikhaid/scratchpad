4966a61edac444189412cec682e52fb0bd777cbc
dominikhaid <info@dominikhaid.de>
2021-08-29 04:30:49 +0200

    fix

--- javascript/class/ex10.js
+++ javascript/class/ex10.js
@@ -1,16 +1,16 @@
 class Person {
-    constructor() {
-        this.name = undefined;
-        this.city = undefined;
-    }
+  constructor() {
+    this.name = undefined;
+    this.city = undefined;
+  }
 }
 
 class Employee extends Person {
-    constructor() {
-        super(); // is a must
-        this.salary = 45000;
-        this.department = undefined;
-    }
+  constructor() {
+    super(); // is a must
+    this.salary = 45000;
+    this.department = undefined;
+  }
 }
 
 let e1 = new Employee();
