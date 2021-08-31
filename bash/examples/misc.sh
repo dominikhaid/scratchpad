c49f86534820af0877908f5b0ac32c5f32703a35
dominikhaid <46886220+dominikhaid@users.noreply.github.com>
2021-09-01 00:34:42 +0200

    usefull commands

---
+++ bash/examples/misc.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+echo "THIS HERE SHOULD BE REPLACED" | sed "s/HERE/\//g"
+echo "HERE_9044REMOVE.jpg" | sed "s/.[a-z|A-Z|0-9|\-]*\.\w*$//g"
+echo "REPLACE HERE" | sd 'HERE' ''
+
+# TRAP IGNORE SIGN / CONDITION
+TR="HALLO
+_
+WELT
+AGAIN"
+echo $TR | tr "_" "\n"
+
+# DISABLE OUTPUT
+stty -echo
+# ENABLE OUTPUT
+stty echo
+
+# GET INPUT FROM USER
+printf "User Name: "
+read USER_NAME
+
+# FIND TEXT FROM THE FIRST COLUMN WITH REGEX
+cat /etc/passwd | awk -F: '{ print $1 }' | awk -e 'match($0, /'$USER_NAME'/) {print substr( $1, RSTART, RLENGTH )}'
+
+# PAUSE SCRIPT
+sleep 5
+
+# SPAWN KEY
+xdotool key c
+
+ls $HOME &
+pid=$!
+# If this pid is killed,, kill the while
+trap "kill $pid 2> /dev/null" EXIT
+
+# execute the function
+while kill -0 $pid 2>/dev/null; do
+	echo "PROCESS LS"
+done
+
+# Disable the trap on a normal exit.
+trap - EXIT
+
+# SORUCE ANOTHER SCRIPT
+#source $SCRIPTPATH
+
+# SET TERMINAL COLOR
+tput setaf 1
+# RESET TERMINAL COLOR
+tput sgr0
