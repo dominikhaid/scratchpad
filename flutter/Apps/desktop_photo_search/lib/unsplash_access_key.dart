49d0872400b1836eadcce14f17da0edaa5ce87d5
dominikhaid <info@dominikhaid.de>
2021-12-08 00:28:39 +0100

    changed  java and flutter paths and names

---
+++ flutter/Apps/desktop_photo_search/lib/unsplash_access_key.dart
@@ -0,0 +1,9 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO: Retrieve an API Access Key from https://unsplash.com/developers
+const String unsplashAccessKey = '2H4Ex239EpZ2-ECYB72eVVZTHNn_R8G6Xv6nPZKdWAw';
+
+// The application name for the above API Access Key.
+const unsplashAppName = 'Flutter';
--- java/readme.md
+++ java/readme.md
@@ -4,3 +4,8 @@
 
 - [reference](https://www.w3schools.com/java/java_ref_keywords.asp)
 - [docs oracle](https://docs.oracle.com/en/java/javase/11/)
+
+## Run / Compile
+
+- javac main.java
+- java Main
