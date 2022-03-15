02a9966d7eda534ca406c232d8f21430d67e7045
dominikhaid <info@dominikhaid.de>
2022-03-16 00:03:49 +0100

    removed

--- python/basics/sys/findFiles.py
+++ python/basics/sys/findFiles.py
@@ -1,6 +1,6 @@
 import fnmatch
 import os
 
-for file in os.listdir("/storage/emulated/0/Download/"):
- if fnmatch.fnmatch(file.upper(), '*.PDF'):
-            print(file)
+for file in os.listdir("/home/dominik"):
+    if fnmatch.fnmatch(file.upper(), '*.LOG'):
+        print(file)
--- python/basics/sys/findRegexInFile.py
+++ python/basics/sys/findRegexInFile.py
@@ -1,6 +1,8 @@
 import re
+
 pattern = re.compile("User conf")
 
-for i, line in enumerate(open('/storage/emulated/0/Documents/github/debian-config/readme.md')):
- for match in re.finditer(pattern, line):
-  print (f"Found {match.group()} on line: {i+1}")
+for i, line in enumerate(
+        open('/storage/emulated/0/Documents/github/debian-config/readme.md')):
+    for match in re.finditer(pattern, line):
+        print(f"Found {match.group()} on line: {i+1}")
